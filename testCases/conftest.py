126770ce7f516b134d28e10b73ed71985f9e7ab2
vishal <vishaldevopser45@gmail.com>
2023-07-05 16:28:23 +0530

    this is my first commit......

---
+++ testCases/conftest.py
@@ -0,0 +1,38 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture()
+def setup(browser):
+    if browser == 'chrome':
+        driver = webdriver.Chrome()
+        print("Launching chrome browser.........")
+    elif browser == 'firefox':
+        driver = webdriver.Firefox()
+        print("Launching firefox browser.........")
+    else:
+        driver = webdriver.Ie()
+    return driver
+
+
+def pytest_addoption(parser):  # This will get the value from CLI /hooks
+    parser.addoption("--browser")
+
+
+@pytest.fixture()
+def browser(request):  # This will return the Browser value to setup method
+    return request.config.getoption("--browser")
+
+
+################## Pytest HTML reports #######################
+
+# def pytest_configure(config):
+#     config._metadata['Project Name'] = 'nop Commerce'
+#     config._metadata['Module Name'] = 'Customers'
+#     config._metadata['Tester'] = 'Pavan'
+#
+#
+# @pytest.mark.optionalhook
+# def pytest_metadata(metadata):
+#     metadata.pop("JAVA_HOME", None)
+#     metadata.pop("plugins", None)
