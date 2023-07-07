fb3aa57b65f9e47623d9fc53f6451c05a60ff50e
vishal <vishaldevopser45@gmail.com>
2023-07-07 12:35:15 +0530

    this is my eigth commits

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-pytest -v -m "sanity" --html=Report/report.html testCases/ --browser chrome
+pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
 rem pytest -v -m "regression" --html=Report/report.html testCases/ --browser chrome
 rem pytest -v -m "sanity or regression" --html=Report/report.html testCases/ --browser chrome
 rem pytest -v -m "sanity and regression" --html=Report/report.html testCases/ --browser chrome
